c3c4584c8040990280aec952b06f6b4dee3e0afa
Martin Pasiecznik <104079011+Martin-Pasiecznik@users.noreply.github.com>
2023-10-18 17:05:19 -0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -7,16 +7,22 @@ public class Calculator{
     Scanner reader = new Scanner(System.in);
     System.out.println("Write a number");
     double num1 = reader.nextDouble();
-    System.out.println("Choose the operation: +,-,*,/ ");
+    System.out.println("Choose the operation: +,-,*,/,^2 ");
     String operation = reader.next();
-    System.out.println("Write second number: ");
-    double num2 = reader.nextDouble();
-
     double result=0;
-    if (operation.equals("+")) result = num1 + num2;
+    result = num1 * num1;
+
+    if (!operation.equals("^2")){
+            System.out.println("Write second number: ");
+            double num2 = reader.nextDouble();
+
+        if (operation.equals("+")) result = num1 + num2;
         else if (operation.equals("-")) result = num1 - num2;
         else if (operation.equals("*")) result = num1 * num2;
         else result = num1 / num2;
+    }
+    
+    
 
     System.out.println("Result: " + result);
 
